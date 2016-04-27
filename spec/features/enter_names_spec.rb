6ebbb7b858bce570c452593a965aa16ee88a9773
hnamitha1 <namitha.h@gmail.com>
2016-04-27 14:47:27 +0100

    Challenge 21

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Slasher'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Huzzah mothafucka!'
+    sign_in_and_play
     expect(page).to have_content 'Slasher vs. Mittens'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "show hit points" do
   scenario "Player 2 displays its hit points" do
-    visit('/')
-    fill_in :player_1_name, with: 'Slasher'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Huzzah mothafucka!'
+    sign_in_and_play
     expect(page).to have_content 'Mittens: 20 Hit Points'
   end
 end
